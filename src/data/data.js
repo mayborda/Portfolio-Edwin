a4586f676118db669e81c80d7bb71b24fe2c774e
May Borda <mayra.borda.4@gmail.com>
2022-03-10 08:49:07 -0300

    datos proyectos web

---
+++ src/data/data.js
@@ -0,0 +1,59 @@
+import port1 from "../img/portImages/port1.png";
+import port2 from "../img/portImages/port2.png";
+import port3 from "../img/portImages/port3.png";
+import port4 from "../img/portImages/port4.png";
+import port6 from "../img/portImages/port5.png";
+import port7 from "../img/portImages/port6.png";
+
+const portfolios = [
+  {
+    id: 1,
+    link: "https://www.santaelena.edu.uy/inicio/",
+    description: "Página oficial del Colegio Santa Elena",
+    icon: "Sitio Web",
+    image: port1,
+    title: "Colegio Santa Elena",
+  },
+  {
+    id: 2,
+    link: "https://e2fitnessgroup.com",
+    description: "Plataforma virtual de deporte",
+    icon: "Sitio Web",
+    image: port2,
+    title: "e2 Fitness Group",
+  },
+  {
+    id: 3,
+    link: "https://macri.uy",
+    description: "Página oficial de la reconocida zapatería en Uruguay",
+    icon: "Sitio Web",
+    image: port3,
+    title: "Macri Zapatería & Sport Center",
+  },
+  {
+    id: 4,
+    link: "https://www.csi-ing.com",
+    description: "Consultora de ingeniería multiespecialista",
+    icon: "Sitio Web",
+    image: port4,
+    title: "CSI Ingenieros",
+  },
+  {
+    id: 5,
+    link: "https://www.infinito.com.uy",
+    description: "Empresa dedicada a salud visual",
+    icon: "Sitio Web",
+    image: port6,
+    title: "Infinito Servicios Ópticos",
+  },
+  {
+    id: 6,
+    link: "https://www.macrisportlife.uy",
+    description: "Tienda online de zapatería deportiva",
+    icon: "Sitio Web",
+    image: port7,
+    title: "Macri Sport Life",
+  },
+];
+
+export default portfolios;
